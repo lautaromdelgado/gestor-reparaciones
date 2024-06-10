63d7b3be0ccba99f58e314add8734105a7cf4982
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-10 17:41:38 -0300

    repair query interface view added

---
+++ InterfazTecnico/InterfazTecnico/VistaDatosConsulta.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace InterfazTecnico
+{
+    partial class VistaDatosConsulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxConsultaMonto = new System.Windows.Forms.TextBox();
+            this.txtboxConsultaTipoReparacion = new System.Windows.Forms.TextBox();
+            this.txtboxConsultaEquipo = new System.Windows.Forms.TextBox();
+            this.txtboxConsultaApellidoNombre = new System.Windows.Forms.TextBox();
+            this.txtboxConsultaDni = new System.Windows.Forms.TextBox();
+            this.lblConsultaMonto = new System.Windows.Forms.Label();
+            this.lblConsultaTipoReparacion = new System.Windows.Forms.Label();
+            this.lblConsultaEquipo = new System.Windows.Forms.Label();
+            this.lblConsultaApellidoNombre = new System.Windows.Forms.Label();
+            this.lblConsultaDni = new System.Windows.Forms.Label();
+            this.lblConsultaDetallesReparacion = new System.Windows.Forms.Label();
+            this.lblConsultaFixify = new System.Windows.Forms.Label();
+            this.btnConsultaSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtboxConsultaMonto
+            // 
+            this.txtboxConsultaMonto.Location = new System.Drawing.Point(206, 330);
+            this.txtboxConsultaMonto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtboxConsultaMonto.Name = "txtboxConsultaMonto";
+            this.txtboxConsultaMonto.Size = new System.Drawing.Size(155, 20);
+            this.txtboxConsultaMonto.TabIndex = 25;
+            // 
+            // txtboxConsultaTipoReparacion
+            // 
+            this.txtboxConsultaTipoReparacion.Location = new System.Drawing.Point(206, 273);
+            this.txtboxConsultaTipoReparacion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtboxConsultaTipoReparacion.Name = "txtboxConsultaTipoReparacion";
+            this.txtboxConsultaTipoReparacion.Size = new System.Drawing.Size(155, 20);
+            this.txtboxConsultaTipoReparacion.TabIndex = 24;
+            // 
+            // txtboxConsultaEquipo
+            // 
+            this.txtboxConsultaEquipo.Location = new System.Drawing.Point(207, 217);
+            this.txtboxConsultaEquipo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtboxConsultaEquipo.Name = "txtboxConsultaEquipo";
+            this.txtboxConsultaEquipo.Size = new System.Drawing.Size(155, 20);
+            this.txtboxConsultaEquipo.TabIndex = 23;
+            // 
+            // txtboxConsultaApellidoNombre
+            // 
+            this.txtboxConsultaApellidoNombre.Location = new System.Drawing.Point(207, 162);
+            this.txtboxConsultaApellidoNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtboxConsultaApellidoNombre.Name = "txtboxConsultaApellidoNombre";
+            this.txtboxConsultaApellidoNombre.Size = new System.Drawing.Size(155, 20);
+            this.txtboxConsultaApellidoNombre.TabIndex = 22;
+            // 
+            // txtboxConsultaDni
+            // 
+            this.txtboxConsultaDni.Location = new System.Drawing.Point(207, 100);
+            this.txtboxConsultaDni.Margin = new System.Windows.Forms.Padding(2);
+            this.txtboxConsultaDni.Name = "txtboxConsultaDni";
+            this.txtboxConsultaDni.Size = new System.Drawing.Size(155, 20);
+            this.txtboxConsultaDni.TabIndex = 21;
+            // 
+            // lblConsultaMonto
+            // 
+            this.lblConsultaMonto.AutoSize = true;
+            this.lblConsultaMonto.Location = new System.Drawing.Point(229, 306);
+            this.lblConsultaMonto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaMonto.Name = "lblConsultaMonto";
+            this.lblConsultaMonto.Size = new System.Drawing.Size(105, 13);
+            this.lblConsultaMonto.TabIndex = 20;
+            this.lblConsultaMonto.Text = "Monto de reparacion";
+            // 
+            // lblConsultaTipoReparacion
+            // 
+            this.lblConsultaTipoReparacion.AutoSize = true;
+            this.lblConsultaTipoReparacion.Location = new System.Drawing.Point(238, 248);
+            this.lblConsultaTipoReparacion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaTipoReparacion.Name = "lblConsultaTipoReparacion";
+            this.lblConsultaTipoReparacion.Size = new System.Drawing.Size(96, 13);
+            this.lblConsultaTipoReparacion.TabIndex = 19;
+            this.lblConsultaTipoReparacion.Text = "Tipo de reparacion";
+            // 
+            // lblConsultaEquipo
+            // 
+            this.lblConsultaEquipo.AutoSize = true;
+            this.lblConsultaEquipo.Location = new System.Drawing.Point(260, 193);
+            this.lblConsultaEquipo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaEquipo.Name = "lblConsultaEquipo";
+            this.lblConsultaEquipo.Size = new System.Drawing.Size(40, 13);
+            this.lblConsultaEquipo.TabIndex = 18;
+            this.lblConsultaEquipo.Text = "Equipo";
+            // 
+            // lblConsultaApellidoNombre
+            // 
+            this.lblConsultaApellidoNombre.AutoSize = true;
+            this.lblConsultaApellidoNombre.Location = new System.Drawing.Point(238, 138);
+            this.lblConsultaApellidoNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaApellidoNombre.Name = "lblConsultaApellidoNombre";
+            this.lblConsultaApellidoNombre.Size = new System.Drawing.Size(92, 13);
+            this.lblConsultaApellidoNombre.TabIndex = 17;
+            this.lblConsultaApellidoNombre.Text = "Apellido y Nombre";
+            // 
+            // lblConsultaDni
+            // 
+            this.lblConsultaDni.AutoSize = true;
+            this.lblConsultaDni.Location = new System.Drawing.Point(274, 85);
+            this.lblConsultaDni.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaDni.Name = "lblConsultaDni";
+            this.lblConsultaDni.Size = new System.Drawing.Size(26, 13);
+            this.lblConsultaDni.TabIndex = 16;
+            this.lblConsultaDni.Text = "DNI";
+            // 
+            // lblConsultaDetallesReparacion
+            // 
+            this.lblConsultaDetallesReparacion.AutoSize = true;
+            this.lblConsultaDetallesReparacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConsultaDetallesReparacion.Location = new System.Drawing.Point(203, 44);
+            this.lblConsultaDetallesReparacion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaDetallesReparacion.Name = "lblConsultaDetallesReparacion";
+            this.lblConsultaDetallesReparacion.Size = new System.Drawing.Size(177, 18);
+            this.lblConsultaDetallesReparacion.TabIndex = 15;
+            this.lblConsultaDetallesReparacion.Text = "Detalles de reparación";
+            this.lblConsultaDetallesReparacion.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblConsultaFixify
+            // 
+            this.lblConsultaFixify.AutoSize = true;
+            this.lblConsultaFixify.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConsultaFixify.Location = new System.Drawing.Point(217, 9);
+            this.lblConsultaFixify.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConsultaFixify.Name = "lblConsultaFixify";
+            this.lblConsultaFixify.Size = new System.Drawing.Size(145, 24);
+            this.lblConsultaFixify.TabIndex = 14;
+            this.lblConsultaFixify.Text = "Fixify Software";
+            // 
+            // btnConsultaSalir
+            // 
+            this.btnConsultaSalir.Location = new System.Drawing.Point(502, 380);
+            this.btnConsultaSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConsultaSalir.Name = "btnConsultaSalir";
+            this.btnConsultaSalir.Size = new System.Drawing.Size(56, 24);
+            this.btnConsultaSalir.TabIndex = 26;
+            this.btnConsultaSalir.Text = "&Atras";
+            this.btnConsultaSalir.UseVisualStyleBackColor = true;
+            this.btnConsultaSalir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // VistaDatosConsulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 415);
+            this.Controls.Add(this.btnConsultaSalir);
+            this.Controls.Add(this.txtboxConsultaMonto);
+            this.Controls.Add(this.txtboxConsultaTipoReparacion);
+            this.Controls.Add(this.txtboxConsultaEquipo);
+            this.Controls.Add(this.txtboxConsultaApellidoNombre);
+            this.Controls.Add(this.txtboxConsultaDni);
+            this.Controls.Add(this.lblConsultaMonto);
+            this.Controls.Add(this.lblConsultaTipoReparacion);
+            this.Controls.Add(this.lblConsultaEquipo);
+            this.Controls.Add(this.lblConsultaApellidoNombre);
+            this.Controls.Add(this.lblConsultaDni);
+            this.Controls.Add(this.lblConsultaDetallesReparacion);
+            this.Controls.Add(this.lblConsultaFixify);
+            this.Name = "VistaDatosConsulta";
+            this.Text = "Datos de reparación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtboxConsultaMonto;
+        private System.Windows.Forms.TextBox txtboxConsultaTipoReparacion;
+        private System.Windows.Forms.TextBox txtboxConsultaEquipo;
+        private System.Windows.Forms.TextBox txtboxConsultaApellidoNombre;
+        private System.Windows.Forms.TextBox txtboxConsultaDni;
+        private System.Windows.Forms.Label lblConsultaMonto;
+        private System.Windows.Forms.Label lblConsultaTipoReparacion;
+        private System.Windows.Forms.Label lblConsultaEquipo;
+        private System.Windows.Forms.Label lblConsultaApellidoNombre;
+        private System.Windows.Forms.Label lblConsultaDni;
+        private System.Windows.Forms.Label lblConsultaDetallesReparacion;
+        private System.Windows.Forms.Label lblConsultaFixify;
+        private System.Windows.Forms.Button btnConsultaSalir;
+    }
+}
